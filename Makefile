62e5c2c1eecace1c558b3bf3ddebf2b195d915ac
Andrew Reeman <andrew.reeman@gmail.com>
2013-11-28 11:37:45 +0000

    Created AudioIO class. Wrapper for portaudio. RAII

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 all: processAudio record
-record: record.o
-	g++ -o record record.o libportaudio.a -lrt -lasound -ljack -lpthread 
-record.o: record.cpp
+record: record.o 
+	g++ -o record record.o AudioIO.o libportaudio.a -lrt -lasound -ljack -lpthread
+record.o: record.cpp AudioIO.cpp
+	g++ -c AudioIO.cpp
 	g++ -c record.cpp
 processAudio: processAudio.o
 	g++ -o processAudio processAudio.o -lsndfile
 processAudio.o: processAudio.cpp
 	g++ -c processAudio.cpp
 clean: 
-	rm -rf *.o record processAudio
+	rm -rf *.o record processAudio 
 	
--- record.cpp
+++ record.cpp
@@ -4,12 +4,17 @@
 //TODO Create a generic driver class that acts as a wrapper around portaudio. But also in the future has the possibility of
 // wrapping other drivers.
 
+
+#include <sndfile.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sndfile.h>
-#include "portaudio.h"
 #include <cmath>
 #include <vector>
+#include <string>
+#include <iostream>
+
+#include "portaudio.h"
+#include "AudioIO.hh"
 
 #define SAMPLE_RATE  (44100)
 #define FRAMES_PER_BUFFER (512)
@@ -17,10 +22,7 @@
 #define NUM_CHANNELS    (2)
 #define PI 3.14159265
 
-/* Select sample format. */
-/* useful to typedef the sample incase we change it in the future */
-#define PA_SAMPLE_TYPE  paFloat32
-typedef float SAMPLE;
+
 
 typedef struct
 {
@@ -40,125 +42,90 @@ void Pa_Cleanup(PaError err){
     fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
 }
 
-// wrapper for the audio driver. To be developed with RAII in mind.
-class AudioDriver{
-    private:
-        int m_numChans;
-        int m_sampleRate;
-        int m_blockSize;
-    public:
-        AudioDriver(int chans, int sRate, int blockSize);
-        ~AudioDriver();
-
-        void write(SAMPLE* input);
-        void read(SAMPLE* output);
-
-        void setChans(int chans);
-        int getChans();
-
-        void setSampleRate(int sampleRate);
-        int getSampleRate();
-
-        void setBlockSize(int blockSize);
-        int getBlockSize();
-
-
-
-
-};
-
 int main(void)
 {
-    PaStreamParameters outputParameters;
-    PaStream* stream;
-    PaError err = paNoError;
-    paTestData data;
-    int totalFrames;
-    int numSamples;
-    int numBytes;
-    SAMPLE val;
-    std::vector<SAMPLE> inputBuffer;
+    try{
+        PaStreamParameters outputParameters;
+        PaStream* stream;
+        PaError err = paNoError;
+        paTestData data;
+        int totalFrames;
+        int numSamples;
+        int numBytes;
+        SAMPLE val;
+        std::vector<SAMPLE> inputBuffer;
 
+        AudioIO test(2, SAMPLE_RATE, FRAMES_PER_BUFFER, "test");
 
+        data.maxFrameIndex = totalFrames = NUM_SECONDS * SAMPLE_RATE;
+        data.frameIndex = 0;
+        numSamples = totalFrames * NUM_CHANNELS;
+        inputBuffer.resize(numSamples, 0.f);
+        /* Data returns a pointer to the internal array. This should only be used if the vector is not later modified
+        leading to the address changing.
+        */
+        data.recordedSamples = inputBuffer.data();
+        // Fill the buffer. At the moment this is a sine wave. In the future it will be sound file data.
+        for(int i=0; i<data.maxFrameIndex; i+=NUM_CHANNELS){
+            data.recordedSamples[i] = sin(  440.f * PI * 2.f * (float)i / (float)SAMPLE_RATE) * 0.3;
+            data.recordedSamples[i+1] = sin(  1.1 * 440.f * PI * 2.f * (float)i / (float)SAMPLE_RATE) * 0.3;
 
-    data.maxFrameIndex = totalFrames = NUM_SECONDS * SAMPLE_RATE;
-    data.frameIndex = 0;
-    numSamples = totalFrames * NUM_CHANNELS;
-    inputBuffer.resize(numSamples, 0.f);
-
-    /* Data returns a pointer to the internal array. This should only be used if the vector is not later modified
-    leading to the address changing.
-    */
-    data.recordedSamples = inputBuffer.data();
-
-    // Fill the buffer. At the moment this is a sine wave. In the future it will be sound file data.
-    for(int i=0; i<data.maxFrameIndex; i+=NUM_CHANNELS){
-        data.recordedSamples[i] = sin(  440.f * PI * 2.f * (float)i / (float)SAMPLE_RATE) * 0.3;
-        data.recordedSamples[i+1] = sin(  1.1 * 440.f * PI * 2.f * (float)i / (float)SAMPLE_RATE) * 0.3;
-
-    }
-
-    err = Pa_Initialize();
-
-
-    outputParameters.device = Pa_GetDefaultOutputDevice();
-    if (outputParameters.device == paNoDevice) {
-        fprintf(stderr,"Error: No default output device.\n");
-        Pa_Terminate();
-        return 1;
-    }
-    outputParameters.channelCount = NUM_CHANNELS;                     /* stereo output */
-    outputParameters.sampleFormat =  PA_SAMPLE_TYPE;
-    outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
-    outputParameters.hostApiSpecificStreamInfo = NULL;
-
-    // Pass the callback function and address of userdata.
-    err = Pa_OpenStream(&stream, NULL, &outputParameters, SAMPLE_RATE,
-              FRAMES_PER_BUFFER, paClipOff, NULL, &data );
-    if( err != paNoError ){
-        Pa_Cleanup(err);
-        return 1;
-    }
-
-    err = Pa_StartStream( stream );
-    if( err != paNoError ){
-        Pa_Cleanup(err);
-        return 1;
-    }
-
-    printf("Waiting for playback to finish.\n"); fflush(stdout);
-
-    // if stream is active then continue streaming.
-    //while( ( err = Pa_IsStreamActive( stream ) )) Pa_Sleep(100);
-
-
-    for(int i=0; i<totalFrames/FRAMES_PER_BUFFER; ++i){
-        err = Pa_WriteStream(stream, &data.recordedSamples[i+data.frameIndex], FRAMES_PER_BUFFER);
-        if(err != paNoError){
+        }
+        err = Pa_Initialize();
+        outputParameters.device = Pa_GetDefaultOutputDevice();
+        if (outputParameters.device == paNoDevice) {
+            fprintf(stderr,"Error: No default output device.\n");
+            Pa_Terminate();
+            return 1;
+        }
+        outputParameters.channelCount = NUM_CHANNELS;                     /* stereo output */
+        outputParameters.sampleFormat =  PA_SAMPLE_TYPE;
+        outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+        outputParameters.hostApiSpecificStreamInfo = NULL;
+        // Pass the callback function and address of userdata.
+        err = Pa_OpenStream(&stream, NULL, &outputParameters, SAMPLE_RATE,
+                  FRAMES_PER_BUFFER, paClipOff, NULL, NULL );
+        if( err != paNoError ){
             Pa_Cleanup(err);
             return 1;
         }
-        data.frameIndex += (FRAMES_PER_BUFFER * NUM_CHANNELS) - 1;
-    }
-    err = Pa_StopStream(stream);
-    if(err != paNoError){
+        err = Pa_StartStream( stream );
+        if( err != paNoError ){
             Pa_Cleanup(err);
             return 1;
+        }
+        printf("Waiting for playback to finish.\n"); fflush(stdout);
+        // if stream is active then continue streaming.
+        //while( ( err = Pa_IsStreamActive( stream ) )) Pa_Sleep(100);
+        for(int i=0; i<totalFrames/FRAMES_PER_BUFFER; ++i){
+            err = Pa_WriteStream(stream, &data.recordedSamples[i+data.frameIndex], FRAMES_PER_BUFFER);
+            if(err != paNoError){
+                Pa_Cleanup(err);
+                return 1;
+            }
+            data.frameIndex += (FRAMES_PER_BUFFER * NUM_CHANNELS) - 1;
+        }
+        err = Pa_StopStream(stream);
+        if(err != paNoError){
+                Pa_Cleanup(err);
+                return 1;
+        }
+        if( err != paNoError ){
+            Pa_Cleanup(err);
+            return 1;
+        }
+        err = Pa_CloseStream( stream );
+        if( err != paNoError ){
+            Pa_Cleanup(err);
+            return 1;
+        }
+        printf("Done.\n"); fflush(stdout);
+        Pa_Terminate();
+        return 0;
     }
-
-    if( err != paNoError ){
-        Pa_Cleanup(err);
+    catch(std::exception e){
+        std::cout << "Exception occurred" <<std::endl;
         return 1;
     }
-
-    err = Pa_CloseStream( stream );
-    if( err != paNoError ){
-        Pa_Cleanup(err);
-        return 1;
-    }
-
-    printf("Done.\n"); fflush(stdout);
-
-    return 0;
 }
 
